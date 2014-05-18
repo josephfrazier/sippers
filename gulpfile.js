eab3a17e8dbf705b5739e2318115356ac3ee5664
Joseph Frazier <joseph@onsip.com>
2014-05-18 15:03:19 -0400

    Use jshint.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var peg = require('gulp-peg');
+var jshint = require('gulp-jshint');
 
 // make gulp work from subdirectories
 process.chdir(__dirname);
@@ -11,10 +12,25 @@ var pegOptions = {
   ]
 };
 
-gulp.task('default', function() {
-  gulp
+var jshintOptions = {
+   laxcomma: true
+  ,laxbreak: true
+};
+
+gulp.task('build', function() {
+  return gulp
     .src('src/sippers.pegjs')
     .pipe(peg(pegOptions).on('error', gutil.log))
     .pipe(gulp.dest('dist'))
+  ;
 });
 
+gulp.task('lint', ['build'], function () {
+  return gulp
+    .src('dist/sippers.js')
+    .pipe(jshint(jshintOptions))
+    .pipe(jshint.reporter('default'))
+  ;
+});
+
+gulp.task('default', ['lint']);
